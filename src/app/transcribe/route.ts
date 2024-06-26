a91e8263f35aefbede0f5e88e702ea3c40457fc4
Bungle <hn.kim@ecubelabs.com>
2024-06-26 09:25:06 +0900

    fix: 재배포

--- src/app/transcribe/route.ts
+++ src/app/transcribe/route.ts
@@ -2,7 +2,9 @@ import { transcribe } from ".";
 
 export async function POST(request: Request) {
   const body = await request.formData();
+  alert(body);
   const audio = body.get("audio") as File;
+  alert(audio);
   const result = await transcribe(audio);
   return new Response(result);
 }
